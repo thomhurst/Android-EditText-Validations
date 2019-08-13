fdfff7a48a3af0805c46356a0316f21d0d7d1d7e
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2019-08-13 19:02:31 +0100

    Migrate to AndroidX

--- app/src/androidTest/java/com/tomlonghurst/edittextvalidator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tomlonghurst/edittextvalidator/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.tomlonghurst.edittextvalidator
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import android.widget.EditText
 import com.tomlonghurst.edittextvalidator.enum.EditTextCondition
 import com.tomlonghurst.edittextvalidator.extensions.*
